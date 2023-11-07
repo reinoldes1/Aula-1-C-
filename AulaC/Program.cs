747303d5157f8b118fe8ee7b2433fdff47959c0f
reinoldes <reinoldesxbox@gmail.com>
2023-11-07 10:51:41 -0300

    Add project files.

---
+++ AulaC/Program.cs
@@ -0,0 +1,56 @@
+﻿// Screen Sound
+
+string mensagemBoasVindas = "Bem vindo ao Screen sound";
+
+void ExibirMensagemBoasVindas()
+{
+    Console.WriteLine(@"
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+
+░██████╗██╗░░░██╗░██████╗████████╗███████╗███╗░░░███╗░██████╗
+██╔════╝╚██╗░██╔╝██╔════╝╚══██╔══╝██╔════╝████╗░████║██╔════╝
+╚█████╗░░╚████╔╝░╚█████╗░░░░██║░░░█████╗░░██╔████╔██║╚█████╗░
+░╚═══██╗░░╚██╔╝░░░╚═══██╗░░░██║░░░██╔══╝░░██║╚██╔╝██║░╚═══██╗
+██████╔╝░░░██║░░░██████╔╝░░░██║░░░███████╗██║░╚═╝░██║██████╔╝
+╚═════╝░░░░╚═╝░░░╚═════╝░░░░╚═╝░░░╚══════╝╚═╝░░░░░╚═╝╚═════╝░
+");
+    Console.WriteLine(mensagemBoasVindas);
+}
+
+void ExibirOpcoesMenu()
+{
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite -1 para sair");
+
+    Console.Write("\nDigite a sua opção: ");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNum = int.Parse(opcaoEscolhida);
+    
+    switch (opcaoEscolhidaNum)
+    {
+        case 1: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNum);
+            break;
+        case 2: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNum);
+            break;
+        case 3: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNum);
+            break;
+        case 4: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNum);
+            break;
+        case -1: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNum);
+            break;
+        default: Console.WriteLine("Opção Inválida");
+            break;
+    }
+}
+
+
+ExibirMensagemBoasVindas();
+ExibirOpcoesMenu();
